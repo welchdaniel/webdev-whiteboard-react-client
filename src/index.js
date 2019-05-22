6fcb05c9bca421fa5a6a6fbfc6bc5db7236a8707
welchdaniel <welch.da@husky.neu.edu>
2019-05-21 20:24:39 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HelloWorld from './components/HelloWorld';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HelloWorld/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

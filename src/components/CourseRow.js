fbd114840890611b27741d0e632039b4e50b916f
welchdaniel <welch.da@husky.neu.edu>
2019-05-23 15:58:38 -0400

    added icons

--- src/components/CourseRow.js
+++ src/components/CourseRow.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileAlt, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 const CourseRow = ({course, selectCourse}) => {
     return(
         <tr>
+            <td>
+                <FontAwesomeIcon icon={faFileAlt} className="text-primary"/>
+            </td>
             <td>
                 <Link 
                     onClick={() => selectCourse(course)}
                     to={`/course/edit/${course.id}`}>
                 {course.title}
                 </Link>
+            </td>
+            <td>
+                me
+            </td>
+            <td>
+                &nbsp;
+            </td>
+            <td>
+                &nbsp;
+            </td>
+            <td>
+                &nbsp;
+            </td>
+            <td>
+                <FontAwesomeIcon icon={faTrashAlt}/>
             </td>
         </tr>
     )
--- src/containers/CourseTable.js
+++ src/containers/CourseTable.js
@@ -1,19 +1,37 @@
 import React from 'react'
 import CourseRow from '../components/CourseRow'
+import { faGripHorizontal, faSortAlphaDown, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const CourseTable = ({courses, selectCourse}) => {
     return(
-        <table className="table">
-            <tbody>
-                { courses.map((course, key) =>
-                    <CourseRow 
-                        selectCourse={selectCourse}
-                        course={course}
-                        key={key}/>
-                    )
-                }
-            </tbody>
-        </table>
+        <div className="table-responsive">
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>&nbsp;</th>
+                    <th>Title</th>
+                    <th>
+                        <a className="pr-2">Owned by</a>
+                        <FontAwesomeIcon icon={faCaretDown} className="fa-md"/>
+                    </th>
+                    <th>Last modified by me</th>
+                    <th><FontAwesomeIcon icon={faGripHorizontal} className="fa-lg"/></th>
+                    <th><FontAwesomeIcon icon={faSortAlphaDown} className="fa-lg"/></th>
+                    <th>&nbsp;</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { courses.map((course, key) =>
+                        <CourseRow 
+                            selectCourse={selectCourse}
+                            course={course}
+                            key={key}/>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
 

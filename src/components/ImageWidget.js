6e85f3cf71422fbdba6dcbe66a4754f023486683
welchdaniel <welch.da@husky.neu.edu>
2019-06-07 01:25:59 -0400

    added arrows to widgets

--- src/components/ImageWidget.js
+++ src/components/ImageWidget.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTrashAlt, faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ImageWidget = ({ widget, deleteWidget }) => {
     return(
         <li className="list-group-item"> 
             {widget.type} 
-            <Button 
-                onClick={() => deleteWidget(widget.id)}
-                variant="danger"
-                className="col-1 float-right">
-                <FontAwesomeIcon icon={faTrashAlt} />
-            </Button>
+            <div align="right">
+                <Button 
+                    onClick={() => deleteWidget(widget.id)}
+                    variant="warning"
+                    className="col-1 mr-2">
+                    <FontAwesomeIcon icon={faArrowUp} />
+                </Button>
+                <Button 
+                    onClick={() => deleteWidget(widget.id)}
+                    variant="warning"
+                    className="col-1 mr-2">
+                    <FontAwesomeIcon icon={faArrowDown} />
+                </Button>
+                <Button 
+                    onClick={() => deleteWidget(widget.id)}
+                    variant="danger"
+                    className="col-1">
+                    <FontAwesomeIcon icon={faTrashAlt} />
+                </Button>
+            </div>
         </li> 
     )
 }
